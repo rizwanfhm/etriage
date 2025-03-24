3e564083e1c21c7c6c4ecdd91d6eb446967017a2
Rizwan Saeed <rizwan.saeed@fullhealthmedical.com>
2025-03-24 11:47:57 +0000

    Set colour for interpretation

--- components/steps/StepResult.tsx
+++ components/steps/StepResult.tsx
@@ -3,12 +3,29 @@ import { StepAttendanceProps } from "@/model/QuestionModel";
 import { Checkbox, Radio, RadioGroup, Select, SelectItem } from "@heroui/react";
 
 export default function StepResult({ result, evaluation }: TriageResult) {
+
+  const resultColour = (() => {
+    switch (evaluation) {
+      case "RED":
+        return "red";
+      case "ORANGE":
+        return "orange";
+      case "YELLOW":
+        return "yellow";
+      case "GREEN":
+        return "green";
+      default:
+        return "blue";
+    };
+  })();
+
+
   return (
     <div className="flex flex-col gap-6">
       <div className="space-y-2">
-        <h2 className="text-2xl font-bold">Result</h2>
-        <p className="text-default-500 text-large">{evaluation} </p>
-        <div className="pt-2">          
+        <h2 className="text-2xl font-bold">Result</h2>      
+        <div className="flex justify-center items-center">
+            <div className="w-20 h-20 flex items-center justify-center" style={{ backgroundColor: resultColour }}></div>
         </div>
       </div>
     </div>
